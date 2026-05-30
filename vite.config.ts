5aa57487b4e8f9c6c1da4af61473d0ac8bc5b2c9
Mark Boline <mwboline@gmail.com>
2026-05-30 13:42:30 -0500

    build: update vite base path to /flashapp_pro/

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/flashapp_pro/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
